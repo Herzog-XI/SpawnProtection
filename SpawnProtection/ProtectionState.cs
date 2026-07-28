3e771f109961196ba1afd9b0829351736f49f334
Herzog-XI <egmherzog@gmail.com>
2026-07-28 14:46:16 +0200

    Remove MEC dependency from protection state

--- SpawnProtection/ProtectionState.cs
+++ SpawnProtection/ProtectionState.cs
@@ -1,16 +1,22 @@
 using System;
-using MEC;
+using System.Threading;
 
 namespace SpawnProtection
 {
-    internal sealed class ProtectionState
+    internal sealed class ProtectionState : IDisposable
     {
         public DateTime FullProtectionEndsAt { get; set; }
         public DateTime TeamProtectionEndsAt { get; set; }
         public bool FullProtectionRemoved { get; set; }
-        public CoroutineHandle TimerCoroutine { get; set; }
+        public Timer Timer { get; set; }
 
         public bool HasFullProtection => !FullProtectionRemoved && DateTime.UtcNow < FullProtectionEndsAt;
         public bool HasTeamProtection => DateTime.UtcNow < TeamProtectionEndsAt;
+
+        public void Dispose()
+        {
+            Timer?.Dispose();
+            Timer = null;
+        }
     }
 }
